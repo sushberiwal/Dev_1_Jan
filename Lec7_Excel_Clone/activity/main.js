1909508aaa365ca3d4c88fa0ad7c5784aba01d3a
Sushant <sushantberiwal@gmail.com>
2021-01-14 19:02:34 +0530

    layout complete

--- Lec7_Excel_Clone/activity/main.js
+++ Lec7_Excel_Clone/activity/main.js
@@ -17,11 +17,15 @@ function createWindow () {
       width: 800,
       height: 600,
       webPreferences: {
-        nodeIntegration: true // node enabled !
+        nodeIntegration: true, // node enabled !,
+        
       }
     })
   
-    win.loadFile('index.html');
+    win.loadFile('index.html').then(function(){
+        win.maximize();
+        win.webContents.openDevTools();
+    })
 }
   
 app.whenReady().then(createWindow)
