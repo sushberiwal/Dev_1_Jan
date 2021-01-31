430a74c1f02051868aee7674ae669bf6292f19e5
Sushant <sushantberiwal@gmail.com>
2021-01-31 20:37:37 +0530

    create user done

--- Lec11_Food_App/activity/app.js
+++ Lec11_Food_App/activity/app.js
@@ -1,18 +1,38 @@
+const fs = require("fs");
 const express = require("express");
 
 // nodejs => code se server build hota tha => express nodejs ke code easy way me
 const app = express();
-
+const usersDb = require("./Model/usersDb.json");
 
 // app . get method , route = localhost:3000/ , function( request , response  )
 
+// adds data in req.body
+app.use(  express.json() );
 
 
-app.get("/home" , function(req , res){
-    // req.body => client se request
 
-    res.send("<h1>Welcome to home page !!</h1>");
-})
+// users
+function createUser(req , res){
+    //console.log("inside create user !!!");
+    let userData = req.body;    
+    if(userData.name){
+        usersDb.push(userData);
+        // console.log(usersDb);
+        fs.writeFileSync("./Model/usersDb.json" , JSON.stringify(usersDb));
+        res.status(200).json({
+            message:"User Created Succesfully !!!"
+        })
+    }else{
+        res.status(204).json({
+            message:"User Data is empty can't create user !!!"
+        })
+    }
+}
+app.post( "/api/user"  , createUser);
+
+
+
 
 
 
