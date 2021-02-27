ecff6a8a2d4cbd709a1c85e51db57e1d4bf1f16e
Sushant <sushantberiwal@gmail.com>
2021-02-27 21:10:14 +0530

    Routing introduced

--- Lec12_Instagram/Frontend/insta/src/App.js
+++ Lec12_Instagram/Frontend/insta/src/App.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
-import Header from './Components/Header/Header';
-import Home from './Components/Home/Home';
+import React, { Component } from "react";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Profile from "./Components/Profile/Profile";
+import Settings from "./Components/Settings/Settings";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return (  <div className="app">
-      <Header />
-      <Home />
-    </div> );
+  state = {};
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <Header />
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/profile" exact>
+              <Profile />
+            </Route>
+            <Route path="/settings" exact>
+              <Settings />
+            </Route>
+            <Route path="*" exact>
+              <Redirect to="/"></Redirect>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
- 
+
 export default App;
--- Lec12_Instagram/Frontend/insta/src/Components/Header/Header.jsx
+++ Lec12_Instagram/Frontend/insta/src/Components/Header/Header.jsx
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
-    state = {  }
-    render() { 
-        return ( <div className="header">
-            <div className="logo">
-                <img src="logo.png" alt=""/>
-            </div>
-            <div className="search-box">
-                <input type="text" name="" placeholder="Search"/>
-            </div>
-            <div className="nav-links">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/profile">Profile</a></li>
-                    <li><a href="/settings">Settings</a></li>
-                    <li><a href="/logout">Logout</a></li>
-                </ul>
-            </div>
-        </div> );
-    }
+  state = {};
+  render() {
+    return (
+      <div className="header">
+        <Link to="/">
+          <div className="logo">
+            <img src="logo.png" alt="" />
+          </div>
+        </Link>
+        <div className="search-box">
+          <input type="text" name="" placeholder="Search" />
+        </div>
+        <div className="nav-links">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Header;
