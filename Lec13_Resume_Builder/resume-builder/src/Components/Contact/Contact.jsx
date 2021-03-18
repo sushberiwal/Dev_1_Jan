a67f3807008f4337016cef3d770b62be393b4a36
Sushant <sushantberiwal@gmail.com>
2021-03-18 19:02:19 +0530

    contact page updated

--- Lec13_Resume_Builder/resume-builder/src/Components/Contact/Contact.jsx
+++ Lec13_Resume_Builder/resume-builder/src/Components/Contact/Contact.jsx
@@ -1,32 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { contactCodes } from "../../util/codes";
 
 class Contact extends Component {
-    state = {  
-        fname:""
-    }
+  state = {
+    codes: [
+      "fname",
+      "lname",
+      "summary",
+      "email",
+      "phone",
+      "profession",
+      "street",
+      "city",
+      "state",
+      "country",
+      "pin",
+    ],
+    contactDetails: {
+      fname: "",
+      lname: "",
+      summary: "",
+      email: "",
+      phone: "",
+      profession: "",
+      street: "",
+      city: "",
+      state: "",
+      country: "",
+      pin: "",
+    },
+  };
 
-    onChangeHandler = (e) =>{
-        let id = e.target.id;
-        let value = e.target.value;
+  onChangeHandler = (e) => {
+    // console.log(contactCodes);
 
-        this.setState({
-            [id]:value
-        })
-    }
+    let id = e.target.id;
+    let value = e.target.value;
 
-    render() { 
-        return ( 
-        <div className="contact-details">
-            <div className="contact-form">
-                <label htmlFor="">Name</label>
-                <input type="text" id="fname" value={this.state.fname} onChange={ (e) => {this.onChangeHandler(e)}}/>
-            </div>
-            <div className="resume-viewer">
-                <h1>{this.state.fname}</h1>
-            </div>
+    this.setState({
+      [id]: value,
+    });
+  };
 
-        </div> );
-    }
+  render() {
+    return (
+      <div className="contact-details">
+        <div className="contact-form">
+          {this.state.codes.map((code) => {
+            return (
+              <div className="contact-form-element" key={code}>
+                <label htmlFor="">{contactCodes[code]}</label>
+                <input
+                  type="text"
+                  id={code}
+                  value={this.state.code}
+                  onChange={(e) => {
+                    this.onChangeHandler(e);
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="resume-viewer">
+          <h1>{this.state.fname}</h1>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Contact;
---
+++ Lec13_Resume_Builder/resume-builder/src/util/codes.js
@@ -0,0 +1,13 @@
+export let contactCodes =  {
+    "fname":"FIRST NAME",
+    "lname":"LAST NAME",
+    "summary":"SUMMARY",
+    "email":"EMAIL",
+    "phone":"PHONE",
+    "profession":"PROFESSION",
+    "street":"STREET",
+    "city":"CITY",
+    "state":"STATE",
+    "country":"COUNTRY",
+    "pin":"PINCODE"
+}
